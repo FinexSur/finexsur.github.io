3b84e6d52b37d9fc12816e3ff86acec0cb1cf3c3
FinexSur <165076443+FinexSur@users.noreply.github.com>
2024-03-31 23:49:31 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleNavbar() {
-     var navbar = document.querySelector('.navbar');
-     if (navbar.className === 'navbar') {
-         navbar.className += ' responsive';
-     } else {
-         navbar.className = 'navbar';
-     }
- }
- 
